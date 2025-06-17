3cfdd8846838013c1b1db88f45720ac4f41b2727
christopher <cloudynginya@gmail.com>
2025-06-16 18:23:57 -0700

    changes

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import { Homepage } from './Components/Homepage/Homepage';
+
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <Homepage/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Homepage/Homepage.jsx
@@ -0,0 +1,20 @@
+
+import React from 'react'
+import './Homepage.css';
+
+export const Homepage = () => {
+  return (
+    <div>
+        <nav className="nav">
+            
+            <ul>
+                <a href="#">Home</a>
+                <a href="#">About us</a>
+                <a href="#">Services</a>
+                <a href="#">Contact</a>
+                <a href="#">Login</a>
+            </ul>
+        </nav>
+    </div>
+  )
+}
